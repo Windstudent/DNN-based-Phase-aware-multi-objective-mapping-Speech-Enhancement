b63c9d7703c93730e9bef2f27b6b98f0aed9c1a5
Wind <2537551101@qq.com>
2019-06-08 17:43:12 +0800

    Add files via upload

---
+++ ReadMe.m
@@ -0,0 +1,23 @@
+
+
+# ------------------------------------------------------------------------------------------------------------
+#  Title:
+#    Phase-Aware-DNNSE-Model
+# 
+#  Discription:
+#
+#    I have trained a FNN denoising model for the clean2noisy FFT and LPS mapping. 
+#    This file contains the processed results for subjective listening tests in 15 noise environments, 
+#    ranging from -5 dB to 15 dB SNR conditions.
+#    
+#  Author:
+#
+#    Written by Wind at Harbin Institute of Technology, Shenzhen on 2019.6.8
+#
+# ---------------------------------------------------------------------------------------------------------------
+
+
+
+
+
+  
